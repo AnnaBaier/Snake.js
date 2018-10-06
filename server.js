c30569205dca4ea38426c2b84ec052ba8b413927
Anna-Marie Baier <anna-marie.baier@live.de>
2018-10-06 15:05:10 +0200

    Enable server logging

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
     console.log(`Incoming Request: ${req.url}`);
     next();
-}) */
+});
 
-app.use('/', express.static(`${__dirname}/public`))
+app.use('/', express.static(`${__dirname}/public`));
   
 app.listen(80, function () {
 console.log('Snake is listening on localhost!');
