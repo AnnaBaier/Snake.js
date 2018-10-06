751e19417fafceb27d387fe6a5196aa675ece315
Anna-Marie Baier <anna-marie.baier@live.de>
2018-10-06 09:45:28 +0200

    Refactor to node.js app

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('https');
+var express = require('express');
+
+var app = express;
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+  
+  app.listen(80, function () {
+    console.log('Snake is listening on localhost!');
+  });
